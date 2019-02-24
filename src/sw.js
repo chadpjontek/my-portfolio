dd5a10e334d4b09f17c58020ce952953cc1afaf8
Chad Pjontek <chadpjontek@gmail.com>
2019-02-24 02:07:54 -0500

    feat: Add open graph meta data

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 // store static cache name in a variable for future app updates
-const staticCacheName = 'chadpjontek-portfolio-v1.1';
+const staticCacheName = 'chadpjontek-portfolio-v1.5';
 
 // install the service worker and cache the files
 self.addEventListener('install', e => {
@@ -24,10 +24,10 @@ self.addEventListener('activate', event => {
     caches.keys().then(cachesNames => {
       return Promise.all(
         cachesNames.filter(cacheName => {
-          return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+          return cacheName.startsWith('chadpjontek-portfolio-') && cacheName != staticCacheName;
         }).map(cacheName => {
           // eslint-disable-next-line no-undef
-          return cache.delete(cacheName);
+          return caches.delete(cacheName);
         })
       );
     })
